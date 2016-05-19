5cff22b5de095650ef24d8958fd3a0d83e91fa3e
Eric Büttner <eric.buettner@tuffz.com>
2016-05-19 15:19:41 +0200

    [UPD] synchronize TrailingSlashRouter class with cakephp 2.7.11 core Router class

--- Routing/TrailingSlashRouter.php
+++ Routing/TrailingSlashRouter.php
@@ -26,21 +26,21 @@ class TrailingSlashRouter extends Router {
 /**
  * Finds URL for specified action.
  *
- * Returns an URL pointing to a combination of controller and action. Param
+ * Returns a URL pointing to a combination of controller and action. Param
  * $url can be:
  *
  * - Empty - the method will find address to actual controller/action.
  * - '/' - the method will find base URL of application.
- * - A combination of controller/action - the method will find url for it.
+ * - A combination of controller/action - the method will find URL for it.
  *
  * There are a few 'special' parameters that can change the final URL string that is generated
  *
- * - `base` - Set to false to remove the base path from the generated url. If your application
- *   is not in the root directory, this can be used to generate urls that are 'cake relative'.
- *   cake relative urls are required when using requestAction.
+ * - `base` - Set to false to remove the base path from the generated URL. If your application
+ *   is not in the root directory, this can be used to generate URLs that are 'cake relative'.
+ *   cake relative URLs are required when using requestAction.
  * - `?` - Takes an array of query string parameters
- * - `#` - Allows you to set url hash fragments.
- * - `full_base` - If true the `FULL_BASE_URL` constant will be prepended to generated urls.
+ * - `#` - Allows you to set URL hash fragments.
+ * - `full_base` - If true the `Router::fullBaseUrl()` value will be prepended to generated URLs.
  *
  * @overwrite
  * @param string|array $url Cake-relative URL, like "/products/edit/92" or "/presidents/elect/4"
@@ -49,13 +49,13 @@ class TrailingSlashRouter extends Router {
  *   and standard URL arguments (indexed array elements)
  * @param bool|array $full If (bool) true, the full base URL will be prepended to the result.
  *   If an array accepts the following keys
- *    - escape - used when making urls embedded in html escapes query string '&'
+ *    - escape - used when making URLs embedded in html escapes query string '&'
  *    - full - if true the full base URL will be prepended.
  * @return string Full translated URL with base path.
  */
 	public static function url($url = null, $full = false) {
-		if (!self::$initialized) {
-			self::_loadRoutes();
+		if (!static::$initialized) {
+			static::_loadRoutes();
 		}
 
 		$params = array('plugin' => null, 'controller' => null, 'action' => 'index');
@@ -67,10 +67,13 @@ class TrailingSlashRouter extends Router {
 		}
 
 		$path = array('base' => null);
-		if (!empty(self::$_requests)) {
-			$request = self::$_requests[count(self::$_requests) - 1];
+		if (!empty(static::$_requests)) {
+			$request = static::$_requests[count(static::$_requests) - 1];
 			$params = $request->params;
 			$path = array('base' => $request->base, 'here' => $request->here);
+		}
+		if (empty($path['base'])) {
+			$path['base'] = Configure::read('App.base');
 		}
 
 		$base = $path['base'];
@@ -78,8 +81,8 @@ class TrailingSlashRouter extends Router {
 
 		if (empty($url)) {
 			$output = isset($path['here']) ? $path['here'] : '/';
-			if ($full && defined('FULL_BASE_URL')) {
-				$output = FULL_BASE_URL . $output;
+			if ($full) {
+				$output = static::fullBaseUrl() . $output;
 			}
 			return $output;
 		} elseif (is_array($url)) {
@@ -111,8 +114,8 @@ class TrailingSlashRouter extends Router {
 				}
 			}
 
-			$prefixExists = (array_intersect_key($url, array_flip(self::$_prefixes)));
-			foreach (self::$_prefixes as $prefix) {
+			$prefixExists = (array_intersect_key($url, array_flip(static::$_prefixes)));
+			foreach (static::$_prefixes as $prefix) {
 				if (!empty($params[$prefix]) && !$prefixExists) {
 					$url[$prefix] = true;
 				} elseif (isset($url[$prefix]) && !$url[$prefix]) {
@@ -127,33 +130,31 @@ class TrailingSlashRouter extends Router {
 
 			$match = false;
 
-			for ($i = 0, $len = count(self::$routes); $i < $len; $i++) {
+			foreach (static::$routes as $route) {
 				$originalUrl = $url;
 
-				if (isset(self::$routes[$i]->options['persist'], $params)) {
-					$url = self::$routes[$i]->persistParams($url, $params);
-				}
+				$url = $route->persistParams($url, $params);
 
-				if ($match = self::$routes[$i]->match($url)) {
+				if ($match = $route->match($url)) {
 					$output = trim($match, '/');
 					break;
 				}
 				$url = $originalUrl;
 			}
 			if ($match === false) {
-				$output = self::_handleNoRoute($url);
+				$output = static::_handleNoRoute($url);
 			}
 			if (empty($extension) && self::$_addTrailingSlash && substr($output, -1, 1) !== '/') {
 				$output .= '/';
 			}
 		} else {
-			if (preg_match('/:\/\/|^(javascript|mailto|tel|sms):|^\#/i', $url)) {
+			if (preg_match('/^([a-z][a-z0-9.+\-]+:|:?\/\/|[#?])/i', $url)) {
 				return $url;
 			}
 			if (substr($url, 0, 1) === '/') {
 				$output = substr($url, 1);
 			} else {
-				foreach (self::$_prefixes as $prefix) {
+				foreach (static::$_prefixes as $prefix) {
 					if (isset($params[$prefix])) {
 						$output .= $prefix . '/';
 						break;
@@ -165,18 +166,18 @@ class TrailingSlashRouter extends Router {
 				$output .= Inflector::underscore($params['controller']) . '/' . $url;
 			}
 		}
-		$protocol = preg_match('#^[a-z][a-z0-9+-.]*\://#i', $output);
+		$protocol = preg_match('#^[a-z][a-z0-9+\-.]*\://#i', $output);
 		if ($protocol === 0) {
 			$output = str_replace('//', '/', $base . '/' . $output);
 
-			if ($full && defined('FULL_BASE_URL')) {
-				$output = FULL_BASE_URL . $output;
+			if ($full) {
+				$output = static::fullBaseUrl() . $output;
 			}
 			if (!empty($extension)) {
 				$output = rtrim($output, '/');
 			}
 		}
-		return $output . $extension . self::queryString($q, array(), $escape) . $frag;
+		return $output . $extension . static::queryString($q, array(), $escape) . $frag;
 	}
 
 }
